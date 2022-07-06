1153b7fc506bc06921935aa94b4ffadd82a4e201
Visakh <56556101+rvishakhs@users.noreply.github.com>
2022-07-06 10:56:34 +0100

    Add files via upload

---
+++ precache-manifest.69ef3ba1dd571ff5cd977784927b0673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b0ff2cb0371de439804",
+    "url": "/static/css/main.b3b54837.chunk.css"
+  },
+  {
+    "revision": "0b0ff2cb0371de439804",
+    "url": "/static/js/main.0b0ff2cb.chunk.js"
+  },
+  {
+    "revision": "80756d56e0454064cb9e",
+    "url": "/static/js/1.80756d56.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b13305cdc61bca4560633ffe9b6d80a4",
+    "url": "/index.html"
+  }
+];
